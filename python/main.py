79bdc6dbff845821c4093bd838ba7cade3566742
Gerry Gubash <ggubash@asu.edu>
2024-05-07 19:20:53 -0700

    added loop

--- python/main.py
+++ python/main.py
@@ -3,13 +3,19 @@
 from w1thermsensor import W1ThermSensor as w1s
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 def main():
 
     sensor1 = w1s()
-    current_temp = sensor1.get_temperature()
-    print("Current Temperature in Celsius:")
-    print(str(current_temp))
+
+    while True:
+        current_temp = sensor1.get_temperature()
+        #print("Current Temperature in Celsius:")
+        print(str(current_temp))
+
+        time.sleep(0.2)
+
 
 if __name__ == "__main__":
 
