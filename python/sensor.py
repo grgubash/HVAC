deb4152a546a8cde7ebdfbf2b4268baac50071ea
senior <ggubash@yahoo.com>
2024-06-24 16:23:51 -0700

    refactor repo

--- python/sensor.py
+++ python/sensor.py
@@ -3,15 +3,27 @@
 from w1thermsensor import W1ThermSensor as w1s
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import random as rnd
 import zmq
 import time
 import sys
 
+def add_topic(topic, message):
+    """
+    Simple function to add a topic to a string to be sent over ZMQ
+    """
+    separator = '::'
+    msg = topic + separator + str(message)
+
+    return msg
 
 def run():
     """
     Main loop of our temperature sensing driver.
     """
+
+    # Specify how often to publish temperature data
+    looptime = 1
 
     # Create our sensor object with the 1W driver library
     sensor1 = w1s()
@@ -30,9 +42,15 @@ def run():
         # Get temperature reading
         temp_data = sensor1.get_temperature()
 
-        # Publish temperature data
-        socket.send_string(topic,(temp_data))
-        time.sleep(1)
+        # Construct a message string to send over ZMQ
+        message = add_topic(topic, temp_data)
+
+        # Publish temperature data to all listeners
+        print(f"Sending: {message} over ZMQ link")
+        socket.send_string(message)
+
+        # Loops are ungoverned, so we have to force a sleep every time or else we will run at 100% computing power
+        time.sleep(looptime)
 
 if __name__ == "__main__":
 
