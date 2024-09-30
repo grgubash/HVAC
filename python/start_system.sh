c225920185a0b3d26faacadace4e4034edf44939
Gerry Gubash <ggubash@asu.edu>
2024-09-29 19:56:05 -0700

    trying to automate, need to test and iterate

--- python/start_system.sh
+++ python/start_system.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-lxterminal -e "./server.py" 
-lxterminal -e "./sensor.py" 
-lxterminal -e "./fanEmulator.py" 
+## lxterminal -e "./server.py" 
+## lxterminal -e "./sensor.py" 
+## lxterminal -e "./fanEmulator.py" 
+
+lxterminal --command="/bin/bash --init-file ./server.py"
+lxterminal --command="/bin/bash --init-file ./sensor.py"
+lxterminal --command="/bin/bash --init-file ./fanEmulator.py"
