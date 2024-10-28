68cff1f1cbdb100eac8de0d3d08d72653e03b95d
Gerry Gubash <ggubash@asu.edu>
2024-10-27 18:48:37 -0700

    added some console output for situational awareness

--- python/relay_test.py
+++ python/relay_test.py
@@ -12,13 +12,17 @@ GPIO.setup(relayPin, GPIO.OUT)
 # Initial State:
 GPIO.output(relayPin, GPIO.LOW)
 
+
 # TURN RELAY ON
+time.sleep(1)
+print('Turning on Relay for 5 seconds...')
 GPIO.output(relayPin, GPIO.HIGH)
 
 # WAIT FIVE SECONDS
 time.sleep(5)
 
 # TURN RELAY OFF
+print('Turning off relay now.')
 GPIO.output(relayPin, GPIO.LOW)
 
 # DO THIS WHEN WE ARE DONE
