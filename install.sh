ec7516c28e8a24e6614d63c5cac59ac166c6b8d9
Gerry Gubash <ggubash@asu.edu>
2025-01-26 13:05:11 -0700

    add systemd template files

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo cp /services/server.service /etc/systemd/system
+sudo cp /services/sensor.service /etc/systemd/system
+sudo cp /services/controller.service /etc/systemd/system
+
+sudo systemctl -daemon-reload
+
+sudo systemctl enable server
+sudo systemctl enable sensor
+sudo systemctl enable controller
+
+sudo systemctl start server
+sudo systemctl start sensor
+sudo systemctl start controller
